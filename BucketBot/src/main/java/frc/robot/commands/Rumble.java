0b9649312aec8054947d240cd318d2ce147c9984
Cdog616 <charsteilen@icloud.com>
2024-02-10 14:35:56 -0600

    Changed rumble command input to DoubleSupplier

--- BucketBot/src/main/java/frc/robot/commands/Rumble.java
+++ BucketBot/src/main/java/frc/robot/commands/Rumble.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -7,8 +9,8 @@ import frc.robot.Constants.OIConstants;
 
 public class Rumble extends Command {
 
-  private final double m_rumble1;
-  private final double m_rumble2;
+  private final DoubleSupplier m_rumble1;
+  private final DoubleSupplier m_rumble2;
   private final XboxController m_joystick = new XboxController(OIConstants.kDriverControllerPort);
   
   /**
@@ -18,7 +20,7 @@ public class Rumble extends Command {
    * @param rumbleL Left rumble motor, slightly more intense than the right
    * @param rumbleR Right rumble motor, slightly less intense than the left
    */
-  public Rumble(double rumbleL, double rumbleR) {
+  public Rumble(DoubleSupplier rumbleL, DoubleSupplier rumbleR) {
     m_rumble1 = rumbleL;
     m_rumble2 = rumbleR;
   }
@@ -32,8 +34,8 @@ public class Rumble extends Command {
   @Override
   public void execute() {
     // For whatever reason the motors are on opposite sides than what is claimed
-    m_joystick.setRumble(RumbleType.kRightRumble, m_rumble1);
-    m_joystick.setRumble(RumbleType.kLeftRumble, m_rumble2);
+    m_joystick.setRumble(RumbleType.kRightRumble, m_rumble1.getAsDouble());
+    m_joystick.setRumble(RumbleType.kLeftRumble, m_rumble2.getAsDouble());
   }
 
   // Called once the command ends or is interrupted.
